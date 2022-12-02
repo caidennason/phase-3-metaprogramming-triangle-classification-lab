2efb15644ab12450357edd3d167cc7e665461133
caiden nason <caidenbnason@gmail.com>
2022-12-02 17:58:29 -0500

    commit tree 35f7e3c753344402cd185786a2c1a8a0264df395

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+
+
+  
+  def initialize(length1, length2, length3)
+    @length_arr = []
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    @length_arr.push(@length1, @length2, @length3)
+  end
+
+  def kind
+    sorted_arr = @length_arr.sort
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      raise TriangleError
+    elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      raise TriangleError
+    elsif @length1 == @length2 && @length1 == @length3 && @length2 == @length3
+      :equilateral 
+    elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
+      :isosceles
+    elsif @length1 != @length2 && @length2 != @length3 && @length1 != @length3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
